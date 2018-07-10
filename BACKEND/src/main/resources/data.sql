2db75ab925c5b905868b50b3b4607121d0d66c15
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-07-10 14:38:40 +0200

    add repository, controller and service for Advertisement model with basic database operations

--- BACKEND/src/main/resources/data.sql
+++ BACKEND/src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO users (email, first_name, last_name, birth_date, registration_date, password, role) VALUES
     ('admin', 'admin', 'user', '1990-01-01', '1990-01-01', 'admin', 'ADMIN');
+
+INSERT INTO advertisements (company_id, name, description) VALUES
+    (1, 'first_company_AD', 'YouWillBuyOurProducts');
