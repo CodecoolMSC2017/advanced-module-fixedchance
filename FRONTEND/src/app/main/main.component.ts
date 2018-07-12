7f539b0aef9fbe6f8643e40e17580c06fb141ef3
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-07-12 14:21:32 +0200

    Add student teacher company panel to main page

--- FRONTEND/src/app/main/main.component.ts
+++ FRONTEND/src/app/main/main.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss',
+              './main.component.anim.scss']
 })
 export class MainComponent implements OnInit {
 
@@ -12,6 +13,12 @@ export class MainComponent implements OnInit {
   isSignUp : boolean = false;
   selectedRole : string;
   prevSelectedRole : Element;
+
+  isStudentOverView : boolean;
+  isTeacherOverView : boolean;
+  isCompanyOverView : boolean;
+
+  
 
   constructor() { }
 
@@ -41,4 +48,61 @@ export class MainComponent implements OnInit {
     console.log(this.selectedRole);
   }
 
+  onEntryHover(event) {
+    const studentElement : HTMLElement = document.getElementById('student-overview');
+    const teacherElement : HTMLElement = document.getElementById('teacher-overview');
+    const companyElement : HTMLElement = document.getElementById('company-overview');
+    event.target.classList.add('overhover');
+    
+    switch (event.target.id) {
+      case 'student-overview':
+        this.isStudentOverView = true;
+        this.isTeacherOverView = false;
+        this.isCompanyOverView = false;
+        teacherElement.classList.add('nonhover');
+        companyElement.classList.add('nonhover');
+        break;
+      case 'teacher-overview':
+        this.isStudentOverView = false;
+        this.isTeacherOverView = true;
+        this.isCompanyOverView = false;
+        studentElement.classList.add('nonhover');
+        companyElement.classList.add('nonhover');
+        break;
+      case 'company-overview':
+        this.isStudentOverView = false;
+        this.isTeacherOverView = false;
+        this.isCompanyOverView = true;
+        studentElement.classList.add('nonhover');
+        teacherElement.classList.add('nonhover');
+        break;
+    }
+  }
+
+  onEntryLeave(event) {
+    const studentElement : HTMLElement = document.getElementById('student-overview');
+    const teacherElement : HTMLElement = document.getElementById('teacher-overview');
+    const companyElement : HTMLElement = document.getElementById('company-overview');
+    event.target.classList.remove('overhover');
+
+    switch (event.target.id) {
+      case 'student-overview':
+        teacherElement.classList.remove('nonhover');
+        companyElement.classList.remove('nonhover');
+        break;
+      case 'teacher-overview':
+        studentElement.classList.remove('nonhover');
+        companyElement.classList.remove('nonhover');
+        break;
+      case 'company-overview':
+        studentElement.classList.remove('nonhover');
+        teacherElement.classList.remove('nonhover');
+        break;
+    }
+    this.isStudentOverView = false;
+    this.isTeacherOverView = false;
+    this.isCompanyOverView = false;
+
+
+  }
 }
