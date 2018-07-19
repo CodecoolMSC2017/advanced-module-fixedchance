65d5291838853425521b60d9e194c03f8bea5400
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-07-19 15:13:45 +0200

    Fix top bar padding

--- FRONTEND/src/app/profile/profile.component.ts
+++ FRONTEND/src/app/profile/profile.component.ts
@@ -18,6 +18,7 @@ export class ProfileComponent implements OnInit {
   percentage : number;
   // stores the date in the right format (yyyy-MM-dd)
   userBirthDate : string;
+  xpNum : number;
 
   constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router, private datePipe: DatePipe) { }
 
@@ -25,16 +26,42 @@ export class ProfileComponent implements OnInit {
     
     this.user = this.dataService.user;
     
-    this.user.experience = 2699;
-    this.user.email = this.dataService.user.email;
-    this.user.firstName = this.dataService.user.firstName;
-    this.user.lastName = this.dataService.user.lastName;
-    this.user.birthDate = this.dataService.user.birthDate;
-    this.user.role = this.user.role;
+    this.user.experience = 1500;
+    
     // convert date to the right format
     this.userBirthDate = this.datePipe.transform(this.user.birthDate,"yyyy-MM-dd");
-
+    
     // xp calculations
+    this.userLevel = 0;
+    this.calculateXp();
+    
+  }
+
+  // the user can add courses if role is teacher or admin
+  isValid() {
+    if (this.user.role == 'TEACHER' || this.user.role == 'ADMIN') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  addXp() {
+    this.user.experience += 100;
+    this.calculateXp();
+  }
+
+  calculateXp() {
+    while (this.user.experience - 1200 - this.userLevel * 300 >= 0) {
+      this.user.experience -= 1200 + this.userLevel * 300;
+      this.userLevel++;
+    }
+    this.currentExp = this.user.experience;
+    this.expToNextLevel = 1200 + this.userLevel * 300;
+    this.percentage = Math.round((this.currentExp / this.expToNextLevel) * 100);
+
+
+    /*
     if (this.user.experience < 1200) {
       this.userLevel = 0;
       this.currentExp = this.user.experience;
@@ -50,15 +77,6 @@ export class ProfileComponent implements OnInit {
       this.currentExp = this.user.experience;
       this.expToNextLevel = 1200 + this.userLevel * 300;
       this.percentage = Math.round((this.currentExp / this.expToNextLevel) * 100);
-    }
-  }
-
-  // the user can add courses if role is teacher or admin
-  isValid() {
-    if (this.user.role == 'TEACHER' || this.user.role == 'ADMIN') {
-      return true;
-    } else {
-      return false;
-    }
+    }*/
   }
 }
