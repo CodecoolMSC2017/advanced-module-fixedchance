d1bb070581513dd18967d8dbf0771a18da5c7c39
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-08-01 15:38:12 +0200

    add company class

---
+++ FRONTEND/src/app/company.ts
@@ -0,0 +1,15 @@
+export class Company {
+    id: number;
+    name: string;
+    username: string;
+    registrationDate: Date;
+    email: string;
+    password: string;
+    active: string;
+    subscription: string;
+    paymentDate: Date;
+    description: string;
+    role: string;
+    enabled: boolean;
+    authorities: String[];
+}
