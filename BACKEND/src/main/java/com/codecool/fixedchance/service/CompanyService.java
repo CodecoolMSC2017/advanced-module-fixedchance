dddc99170a7aa3af363db8dcecb03c6cb9f2cb54
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-08-01 15:36:13 +0200

    rewrite add method

--- BACKEND/src/main/java/com/codecool/fixedchance/service/CompanyService.java
+++ BACKEND/src/main/java/com/codecool/fixedchance/service/CompanyService.java
@@ -2,6 +2,7 @@ package com.codecool.fixedchance.service;
 
 import com.codecool.fixedchance.domain.Company;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Component;
@@ -31,7 +32,7 @@ public class CompanyService extends AbstractService {
         return companyRepository.findByEmail(email);
     }
 
-    public Company getCompanyByUsername(String username) {
+    public Company getCompanyByName(String username) {
         return companyRepository.findByUsername(username);
     }
 
@@ -39,25 +40,29 @@ public class CompanyService extends AbstractService {
         companyRepository.save(company);
     }
 
+    // TODO: find a better solution
     @Transactional
     public void add(String email, String username, String name, String password,
                     String confirmationPassword, String role, String subscription) {
         if (!password.equals(confirmationPassword)) {
             throw new IllegalArgumentException();
         }
-       /* userDetailsManager.createUser(new org.springframework.security.core.userdetails.User(
+
+        userDetailsManager.createUser(new org.springframework.security.core.userdetails.User(
                 username,
                 passwordEncoder.encode(password),
                 AuthorityUtils.createAuthorityList("ROLE_" + role)));
-       */
+
         Company company = new Company();
-        company.setUserName(username);
+        company.setUsername(username);
         company.setPassword(passwordEncoder.encode(password));
         company.setEmail(email);
         company.setName(name);
         company.setRegistrationDate(new Date());
         company.setSubscription(subscription);
         company.setActive(true);
+        company.setAuthorities(List.of("ROLE_" + role));
+        company.setEnabled(true);
         companyRepository.save(company);
     }
 
