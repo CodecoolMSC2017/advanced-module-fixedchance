e04101a3334cc14fef5d4ca8f6b803adedb168f3
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-08-22 19:04:54 +0200

    Remove unused imports

--- BACKEND/src/main/java/com/codecool/fixedchance/controller/AuthController.java
+++ BACKEND/src/main/java/com/codecool/fixedchance/controller/AuthController.java
@@ -3,8 +3,6 @@ package com.codecool.fixedchance.controller;
 import com.codecool.fixedchance.domain.Company;
 import com.codecool.fixedchance.domain.SimpleUser;
 import com.codecool.fixedchance.domain.User;
-import com.codecool.fixedchance.service.SimpleUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +14,6 @@ import java.security.Principal;
 @RestController
 @RequestMapping("/auth")
 public class AuthController extends AbstractController {
-
-    @Autowired
-    private SimpleUserService simpleUserService;
 
     @GetMapping("")
     public SimpleUser get(Principal principal) {
