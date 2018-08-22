e5ab8835c5d1ffcf7e9026a169fa6037422e84e8
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-08-22 18:51:20 +0200

    Add new exception

---
+++ BACKEND/src/main/java/com/codecool/fixedchance/exception/UserAlreadyExistsException.java
@@ -0,0 +1,16 @@
+package com.codecool.fixedchance.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "This username is already in use!")
+public class UserAlreadyExistsException extends Exception {
+
+    public UserAlreadyExistsException() {
+    }
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
