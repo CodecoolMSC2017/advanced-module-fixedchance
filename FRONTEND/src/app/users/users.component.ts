79c33d65167c6a1334d5084f60f9edfc3f2d56cc
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-08-28 11:47:35 +0200

    Fix visibility issue

--- FRONTEND/src/app/users/users.component.ts
+++ FRONTEND/src/app/users/users.component.ts
@@ -6,6 +6,7 @@ import { DatePipe } from '@angular/common';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { OrderPipe } from 'ngx-order-pipe';
+import { Company } from '../company';
 
 @Component({
   selector: 'app-users',
@@ -17,41 +18,58 @@ export class UsersComponent implements OnInit {
   users: UserEntry[];
   usersProto: UserEntry[];
   user: User;
+  company: Company;
   contentLoaded: boolean;
   order: string;
-  cnt: number = 0;
+  cnt = 0;
   toSearch: string;
 
-  constructor(private dataService: DataService, private datePipe: DatePipe, private authService: AuthService, private router: Router, private orderPipe: OrderPipe) { }
+  constructor(private dataService: DataService,
+    private datePipe: DatePipe,
+    private authService: AuthService,
+    private router: Router,
+    private orderPipe: OrderPipe) {
+    }
 
   ngOnInit() {
-    this.authService.getAuth().subscribe(resp => {
-      this.user = resp;
-      this.dataService.fetchUsers().subscribe(users => {
-        this.users = users;
-        this.usersProto = this.users;
-        this.users.forEach(element => {
-          element.level = this.dataService.calculateLevel(element.experience);
-          element.regDate = this.datePipe.transform(element.registrationDate, 'yyyy-MM-dd');
-          //
-          let experience = element.experience;
-          while (experience - 1200 - element.level * 300 >= 0) {
-            experience -= 1200 + element.level * 300;
-          }
-          let currentExp = experience;
-          let expToNextLevel = 1200 + element.level * 300;
-          element.percentage = Math.round((currentExp / expToNextLevel) * 100);
-          //
-          element.role = element.role.substring(5, element.role.length);
-          this.contentLoaded = true;
+    this.authService.getAuth().subscribe(user => {
+      this.user = user;
+      if (this.user !== null) {
+        this.showUsers();
+      } else {
+        this.authService.getAuthCompany().subscribe(company => {
+          this.company = company;
+          this.showUsers();
         });
+      }
+    });
+  }
+
+  showUsers() {
+    this.dataService.fetchUsers().subscribe(users => {
+      this.users = users;
+      this.usersProto = this.users;
+      this.users.forEach(element => {
+        element.level = this.dataService.calculateLevel(element.experience);
+        element.regDate = this.datePipe.transform(element.registrationDate, 'yyyy-MM-dd');
+        //
+        let experience = element.experience;
+        while (experience - 1200 - element.level * 300 >= 0) {
+          experience -= 1200 + element.level * 300;
+        }
+        const currentExp = experience;
+        const expToNextLevel = 1200 + element.level * 300;
+        element.percentage = Math.round((currentExp / expToNextLevel) * 100);
+        //
+        element.role = element.role.substring(5, element.role.length);
+        this.contentLoaded = true;
       });
     });
   }
 
   onUserClicked(event) {
-    let userName = event.target.id;
-    if (userName === this.user.user.username) {
+    const userName = event.target.id;
+    if (this.user !== null && userName === this.user.user.username) {
       this.router.navigate(['profile']);
     } else {
       this.router.navigate(['users/' + userName]);
